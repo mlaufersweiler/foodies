137d8eadbbfe4610bfaeb37d752a0a7f0d1cda1f
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-09-25 14:09:20 -0400

    users table sql statements

---
+++ db/seeds.sql
@@ -0,0 +1,36 @@
+create table users (
+    user_id serial primary key, 
+    name varchar,
+    email varchar,
+    password varchar
+);
+
+create table foods (
+    food_id serial primary key,
+    food_name varchar,
+    calories int,
+    carbs int,
+    protein int,
+    fat int,
+    fiber int,
+    sugar int,
+    quantity int,
+    unit varchar,
+    meal_id int references meals(meal_id)
+);
+
+create table meals (
+    meal_id serial primary key,
+    date_created varchar, 
+    meal_number int,
+    foods text,
+    user_id int references users(user_id)
+);
+
+create table weights (
+    weight_id serial primary key,
+    pounds int,
+    date_created varchar,
+    exact_date varchar,
+    user_id int references users
+);
